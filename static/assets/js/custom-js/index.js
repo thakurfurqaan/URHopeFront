459923fff3c0cecb8225f142ad8d9d91cbd12402
furqaan <thakurfurqaan@gmail.com>
2020-05-01 13:26:47 +0530

    Only Register, login and few other pages remaining

--- static/assets/js/custom-js/index.js
+++ static/assets/js/custom-js/index.js
@@ -1,10 +1,16 @@
 //======================AUTOCOMPLETE=============================
+
+// import { arr } from 'POTABLE.js';
+
 function autocomplete(inp, arr) {
+    // console.log("autocomplete");
+    // console.log(arr);
     /*the autocomplete function takes two arguments,
     the text field element and an array of possible autocompleted values:*/
     var currentFocus;
     /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function (e) {
+        // console.log("autocomplete");
         var a, b, i, val = this.value;
         /*close any already open lists of autocompleted values*/
         closeAllLists();
@@ -19,18 +25,23 @@ function autocomplete(inp, arr) {
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
+        var count = 0;
         for (i = 0; i < arr.length; i++) {
+
             /*check if the item starts with the same letters as the text field value:*/
-            // if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            if (arr[i].toUpperCase().includes(val.toUpperCase())) {
+            // if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() && count < 5) {
+
+            if (arr[i].toUpperCase().includes(val.toUpperCase()) && count < 50) {
                 /*create a DIV element for each matching element:*/
+                count++;
+                // console.log(count);
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
                 var index = arr[i].toUpperCase().indexOf(val.toUpperCase());
                 var len_of_arr = arr[i].toUpperCase().length;
                 b.innerHTML = arr[i].substr(0, index);
                 b.innerHTML += "<strong>" + arr[i].substr(index, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(index+val.length, len_of_arr);
+                b.innerHTML += arr[i].substr(index + val.length, len_of_arr);
                 /*insert a input field that will hold the current array item's value:*/
                 b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
@@ -42,7 +53,16 @@ function autocomplete(inp, arr) {
                     closeAllLists();
                 });
                 a.appendChild(b);
+                if (count === 50) {
+                    b = document.createElement("DIV");
+                    /*make the matching letters bold:*/
+                    var index = arr[i].toUpperCase().indexOf(val.toUpperCase());
+                    var len_of_arr = arr[i].toUpperCase().length;
+                    b.innerHTML = "Showing only top 50 results";
+                    a.appendChild(b);
+                }
             }
+
         }
     });
     /*execute a function presses a key on the keyboard:*/
@@ -109,8 +129,11 @@ function autocomplete(inp, arr) {
 /*An array containing all the country names in the world:*/
 var countries = ['400009 - Mumbai, Maharashtra', "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre & Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts & Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
 
+autocomplete(document.getElementById("myInput"), array_big);
+
+// autocomplete(document.getElementById("myInput"), countries);
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
-autocomplete(document.getElementById("myInput"), countries);
+// autocomplete(document.getElementById("myInput"), countries);
 
 //======================END AUTOCOMPLETE=============================
 
